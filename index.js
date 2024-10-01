437477cfc47d9f12100ca3ec6713cdef76694b9d
César Lizurey <the_dj_caesar@hotmail.com>
2024-10-01 15:22:55 +0200

    JSON options & array sorting

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
-function rowToTree(data, idKey = 'id', parentKey = 'id_parent', childrenKey = 'children') {
-    const map = {};
-    const roots = [];
-  
-    data.forEach((item) => {
-      const newItem = { ...item, [childrenKey]: [] };
-      map[newItem[idKey]] = newItem;
-      const parent = map[newItem[parentKey]];
-      if (parent) {
-        parent[childrenKey].push(newItem);
-      } else {
-        roots.push(newItem);
-      }
-    });
-  
-    return roots;
-  }
-
-  module.exports = {
-    rowToTree
+function rowToTree(data, options = {}) {
+  const defaultOptions = {
+    idKey: 'id',
+    parentKey: 'id_parent',
+    childrenKey: 'children'
   };
+  const mergedOptions = { ...defaultOptions, ...options };
+
+  // Row sort so that rows are treated in order
+  data.sort((a, b) => {
+    if (a[mergedOptions.parentKey] === null) return -1;
+    if (b[mergedOptions.parentKey] === null) return 1;
+    return a[mergedOptions.parentKey] - b[mergedOptions.parentKey];
+  });
+
+  const map = {};
+  const roots = [];
+
+  data.forEach((item) => {
+    const newItem = { ...item, [mergedOptions.childrenKey]: [] };
+    map[newItem[mergedOptions.idKey]] = newItem;
+    const parent = map[newItem[mergedOptions.parentKey]];
+    if (parent) {
+      parent[mergedOptions.childrenKey].push(newItem);
+    } else {
+      roots.push(newItem);
+    }
+  });
+
+  /*
+    There may be several roots, that's why an Array is returned.
+    The roots are sorted by their id, in order to prevent a random order of the items
+  */
+  return roots.sort((a,b) => a[mergedOptions.idKey] > b[mergedOptions.idKey] ? 1 : -1);
+}
+
+module.exports = {
+  rowToTree
+};
--- tests/index.test.js
+++ tests/index.test.js
@@ -47,4 +47,105 @@ describe('rowToTree', () => {
 
     expect(rowToTree(data)).toEqual(expected);
   });
+
+  it('should take into account all the options', () => {
+    const data = [
+      { ident: 1, idParent: null, name: "ROOT" },
+      { ident: 2, idParent: 1, name: "LEAF 2" },
+      { ident: 4, idParent: 1, name: "LEAF SISTER" },
+      { ident: 6, idParent: 2, name: "OTHER LEAF" },
+      { ident: 5, idParent: null, name: "NEW ROOT" },
+    ];
+
+    const expected = [
+      {
+        ident: 1,
+        idParent: null,
+        name: "ROOT",
+        sons: [
+          {
+            ident: 2,
+            idParent: 1,
+            name: "LEAF 2",
+            sons: [
+              {
+                ident: 6,
+                idParent: 2,
+                name: "OTHER LEAF",
+                sons: []
+              }
+            ]
+          },
+          {
+            ident: 4,
+            idParent: 1,
+            name: "LEAF SISTER",
+            sons: []
+          }
+        ]
+      },
+      {
+        ident: 5,
+        idParent: null,
+        name: "NEW ROOT",
+        sons: []
+      },
+    ];
+
+    expect(rowToTree(data, {
+      idKey: 'ident',
+      parentKey: 'idParent',
+      childrenKey: 'sons'
+    })).toEqual(expected);
+  });
+
+  it('should take into account partial options', () => {
+    const data = [
+      { id: 1, idParent: null, name: "ROOT" },
+      { id: 2, idParent: 1, name: "LEAF 2" },
+      { id: 4, idParent: 1, name: "LEAF SISTER" },
+      { id: 6, idParent: 2, name: "OTHER LEAF" },
+      { id: 5, idParent: null, name: "NEW ROOT" },
+    ];
+
+    const expected = [
+      {
+        id: 1,
+        idParent: null,
+        name: "ROOT",
+        sons: [
+          {
+            id: 2,
+            idParent: 1,
+            name: "LEAF 2",
+            sons: [
+              {
+                id: 6,
+                idParent: 2,
+                name: "OTHER LEAF",
+                sons: []
+              }
+            ]
+          },
+          {
+            id: 4,
+            idParent: 1,
+            name: "LEAF SISTER",
+            sons: []
+          }
+        ]
+      },
+      {
+        id: 5,
+        idParent: null,
+        name: "NEW ROOT",
+        sons: []
+      },
+    ];
+
+    expect(rowToTree(data, {
+      parentKey: 'idParent',
+      childrenKey: 'sons'
+    })).toEqual(expected);
+  });
 });
